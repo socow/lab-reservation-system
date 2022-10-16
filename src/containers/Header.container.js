7306b6318f0326d6611489eefebd249aa27633fb
ebchoi <eunbichoi1@gmail.com>
2022-10-16 15:13:16 +0900

    [Add] 데스크탑, 태블릿, 모바일 반응형 레이아웃 적용

--- src/containers/Header.container.js
+++ src/containers/Header.container.js
@@ -1,28 +1,61 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import { Nav, WebNav } from '../components/_index.components';
+import { colors, device } from '../styles/Theme';
 
 export const Header = () => {
   return (
     <Container>
-      <WebNav />
-      <ContentsWrapper>
-        <Nav />
-        <Contents>
-          <Outlet />
-        </Contents>
-      </ContentsWrapper>
+      <Inner>
+        <WebNav />
+        <ContentsWrapper>
+          <Nav />
+          <Contents>
+            <Outlet />
+          </Contents>
+        </ContentsWrapper>
+      </Inner>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  background-color: ${colors.black};
+`;
+
+const Inner = styled.div`
+  margin: 0 auto;
+  width: 100%;
+
+  ${device.tablet} {
+    min-width: 420px;
+    display: flex;
+    justify-content: center;
+  }
+
+  ${device.desktop} {
+    width: 80%;
+    max-width: 1200px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
 const ContentsWrapper = styled.div`
-  background-color: orange;
-  border: 1px solid orange;
+  background-color: ${colors.lightgray};
+  min-width: 375px;
+
+  ${device.tablet} {
+    width: 420px;
+    height: 100vh;
+  }
+  ${device.desktop} {
+    width: 420px;
+    height: 100vh;
+  }
 `;
 
 const Contents = styled.div`
-  background-color: lightpink;
-  border: 1px solid red;
+  border: 3px solid green;
 `;
